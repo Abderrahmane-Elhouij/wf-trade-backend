9fba847ee66145ddf54164dbf20c3c1e4c086b4c
Abderrahmane El-houij <abdoel132003@gmail.com>
2025-07-01 20:20:33 +0100

    refactor: remove unused methods from InitOperationService and InitOperationServiceImpl

--- src/main/java/com/example/wfinittrade/service/facade/InitOperationService.java
+++ src/main/java/com/example/wfinittrade/service/facade/InitOperationService.java
@@ -6,41 +6,9 @@ import com.example.wfinittrade.controller.dto.OperationByProductTypeDto;
 import com.example.wfinittrade.controller.dto.OperationStatusDistributionDto;
 import com.example.wfinittrade.model.InitOperation;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 public interface InitOperationService {
-
-    long getTotalOperations();
-
-    BigDecimal getTotalAmountMAD();
-
-    long getDraftCount();
-
-    long getPendingDecisionCount();
-
-    long getUniqueClientsCount();
-
-    BigDecimal getMaxClientBalance();
-
-    long getVipClientsCount();
-
-    long getMissingBICCount();
-
-    long getExpiredOperationsCount();
-
-    long getCancelledOpsCount();
-
-    long getAmendedOpsCount();
-
-    long getWorkflowAttachedCount();
-
-    long getAwaitingDecisionCount();
-
-    long getCompletedCount();
-
-    List<InitOperation> getOperationsBetween(LocalDate start, LocalDate end);
 
     List<InitOperation> getAllOperations();
 
